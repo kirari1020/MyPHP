e3b2d6e8d9664de9597c0270a2c91abf804e30db
kirari1020 <benkira1020@gmail.com>
2021-08-01 17:46:22 +0900

    practice

--- lesson06_01.php
+++ lesson06_01.php
@@ -1,8 +1,13 @@
 <?php
-$x = 10;
-$y = 2;
-$z = 10;
+$age = 5;
 
-echo ($x <= $y)."\n";
-echo ($x >= $z)."\n";
+if ($age >= 10 && $age < 20) {
+  echo ("10代");
+}else if ($age >= 20 && $age < 30) {
+  echo("20代");
+}else if ($age >= 30 && $age < 40) {
+  echo("30代");
+}else {
+ echo ("それ以外");
+}
 
